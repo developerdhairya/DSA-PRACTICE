bc394c5b8e40c766f1fb57f37084f5e1325b6273
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-01-26 16:51:42 +0000

    Sync LeetCode submission - Single Number III (java)

--- prev_leet/problems/single_number_iii/solution.java
+++ prev_leet/problems/single_number_iii/solution.java
@@ -1,22 +1,25 @@
 class Solution {
     public int[] singleNumber(int[] arr) {
-        Arrays.sort(arr);
+        int N=arr.length;
         int[] output_arr=new int[2];
-        int p=0;
-        for(int i=0;i<arr.length;i++){
-            if(i!=arr.length-1 && arr[i]==arr[i+1]){
-                i++;
+        
+        int xor_arr=0;
+        
+        for(int i=0;i<=N-1;i++){
+            xor_arr^=arr[i];
+        }
+        
+        int lowest_on_bit=xor_arr&(-xor_arr);
+        
+        for(int i=0;i<=N-1;i++){
+            if((arr[i] & lowest_on_bit)==0){
+                output_arr[0]^=arr[i];
             }else{
-                output_arr[p]=arr[i];
-                p++;
-                if(p==2){
-                    break;
-                }
+                output_arr[1]^=arr[i];
             }
         }
+        
         return output_arr;
+        
     }
 }
-
-
-// 1,1,2,2,3,5
