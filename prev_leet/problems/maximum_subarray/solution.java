ab13a585add8b7e6b4de54abf497fed16e8f7675
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-09-19 09:24:30 +0000

    Sync LeetCode submission - Maximum Subarray (java)

--- prev_leet/problems/maximum_subarray/solution.java
+++ prev_leet/problems/maximum_subarray/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maxSubArray(int[] arr) {
         int n=arr.length;
-        int i=1;
-        int csum=arr[0];
-        int sum=csum;
-
-        while(i<n){
-            if(arr[i]>arr[i]+csum){
-                csum=arr[i];
-            }else{
+        int sum=Integer.MIN_VALUE;
+        int csum=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]+csum>arr[i]){
                 csum+=arr[i];
+            }else{
+                csum=arr[i];
             }
             sum=Math.max(sum,csum);
-            i++;
         }
-        return sum;
+            return sum;
+        
     }
 }
