4fedc038e70a1f57f3de1694e331b59527eb85d6
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-09-26 19:35:39 +0000

    Sync LeetCode submission - Capacity To Ship Packages Within D Days (java)

---
+++ prev_leet/problems/capacity_to_ship_packages_within_d_days/solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public static int countSt(int[] arr,int N,int M,int mid){
+        int output=1;
+        int currsum=0;
+        for(int ele:arr){
+            // if(ele>mid){
+            //     return Integer.MAX_VALUE;
+            // }
+            currsum+=ele;
+            if(currsum>mid){
+                currsum=ele;
+                output++;
+            }
+        }
+        return output;
+    }
+    
+    public int shipWithinDays(int[] arr, int M) {
+        int N=arr.length;
+        if(N<M){
+            return -1;
+        }
+        
+        int start=0;
+        for(int ele:arr){
+            start=Math.max(ele,start);
+        }
+        int end=0;
+        for(int ele:arr){
+            end+=ele;
+        }
+        int result=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int stCount=countSt(arr,N,M,mid);
+            if(stCount<=M){
+                result=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return result;
+    }
+}
