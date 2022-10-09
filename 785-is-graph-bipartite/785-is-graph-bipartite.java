39756ad5b3d728ca4e1f03f5ade2e7c202a66966
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-10-09 15:55:24 +0530

    Time: 20 ms (5.38%), Space: 53.5 MB (62.04%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isBipartite(int[][] adjList) {
+        int V=adjList.length;
+        int[] colored=new int[V];
+        Arrays.fill(colored,-1);
+        boolean[] visited=new boolean[adjList.length];
+        boolean ans=true;
+        for(int i=0;i<adjList.length;i++){
+            Arrays.fill(colored,-1);
+            ans&=canColor(adjList,colored,i,0,visited);
+            if(!ans){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean canColor(int[][] adjList,int[] colored,int v,int c,boolean[] visited){
+        
+        visited[v]=true;
+        
+        if(colored[v]==c){
+            return true;
+        }
+        
+        if(colored[v]==1-c){
+            return false;
+        }
+        colored[v]=c;
+        boolean output=true;
+        
+        
+        for(int j=0;j<adjList[v].length;j++){
+            output&=canColor(adjList,colored,adjList[v][j],1-c,visited);
+            if(!output){
+                return false;
+            }
+        }
+        
+        return output;
+        
+    }}
