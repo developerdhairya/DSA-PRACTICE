bd0df5c47be328d752f07a97715eb18ed43d8b81
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-10-24 01:59:29 +0530

    Time: 33 ms (52.36%), Space: 44.8 MB (69.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -3,8 +3,9 @@ class Solution {
         int[] tab=new int[change+1];
         Arrays.fill(tab,Integer.MAX_VALUE);
         tab[0]=0;
+            for(int i=0;i<change+1;i++){
+        
         for(int ele:coins){
-            for(int i=ele;i<change+1;i++){
                 if(i-ele>=0 && tab[i-ele]<Integer.MAX_VALUE)
                     tab[i]=Math.min(tab[i],tab[i-ele]+1);
             }
