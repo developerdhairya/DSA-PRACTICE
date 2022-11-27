123dd95d44f43a94967404a9df7e5b66c893268b
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-11-27 14:40:28 +0530

    Added solution - LeetHub

--- Pascal Triangle - GFG/pascal-triangle.java
+++ Pascal Triangle - GFG/pascal-triangle.java
@@ -30,18 +30,19 @@ class GFG {
 
 // User function Template for Java
 
+
 class Solution {
     ArrayList<Long> nthRowOfPascalTriangle(int n) {
         ArrayList<Long> list=new ArrayList<Long>();
         long[][] temp=new long[n+1][n+1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                if(j==1 || j==i){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
                     temp[i][j]=1;
                 }else{
                     temp[i][j]=(temp[i-1][j-1]+temp[i-1][j])%1000000007;
                 }
-                if(i==n) list.add(temp[i][j]);
+                if(i==n-1) list.add(temp[i][j]);
             }
         }
         return list;
