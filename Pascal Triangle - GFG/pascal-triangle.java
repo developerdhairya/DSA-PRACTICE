c6d3f7ec465f37d8245c5d492266c5295add01ec
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-11-27 13:53:56 +0530

    Added solution - LeetHub

---
+++ Pascal Triangle - GFG/pascal-triangle.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            ArrayList<Long> ans = new Solution().nthRowOfPascalTriangle(n);
+            printAns(ans);
+        }
+    }
+    public static void printAns(ArrayList<Long> ans) {
+        for (Long x : ans) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    ArrayList<Long> nthRowOfPascalTriangle(int n) {
+        ArrayList<Long> list=new ArrayList<Long>();
+        long[][] temp=new long[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(j==1 || j==i){
+                    temp[i][j]=1;
+                }else{
+                    temp[i][j]=(temp[i-1][j-1]+temp[i-1][j])%1000000007;
+                }
+                if(i==n) list.add(temp[i][j]);
+            }
+        }
+        return list;
+    }
+}
