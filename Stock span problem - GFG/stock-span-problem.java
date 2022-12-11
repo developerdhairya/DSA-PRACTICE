49f5193754ba12d1b3c862291f6e3050c36ad42e
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-12-11 21:34:00 +0530

    Added solution - LeetHub

---
+++ Stock span problem - GFG/stock-span-problem.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int a[] = new int[n];
+            
+            int i = 0;
+            for(i = 0; i < n; i++)
+              a[i] = sc.nextInt();
+              
+            int[] s = new Solution().calculateSpan(a, n);
+            
+            for(i = 0; i < n; i++)
+            {
+                System.out.print(s[i] + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    
+    
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to calculate the span of stockâ€™s price for all n days.
+    public static int[] calculateSpan(int arr[], int n)
+    {
+        int[] output=new int[arr.length];
+        int c=0;
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && arr[stack.peek()]<=arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                output[c++]=i-(-1);
+            }else{
+                output[c++]=i-stack.peek();
+            }
+            stack.push(i);
+        }
+        return output;
+    }
+    
+}
