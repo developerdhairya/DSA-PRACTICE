1750daa8692faf0535e49811978ba7dbc4b5997a
Dhairya Taneja <74733831+developerdhairya@users.noreply.github.com>
2022-12-29 21:29:30 +0530

    Added solution - LeetHub

---
+++ Delete a node from BST - GFG/delete-a-node-from-bst.java
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            String s = br.readLine();
+            int x = Integer.parseInt(br.readLine());
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            root = g.deleteNode(root, x);
+            printInorder(root);
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Tree {
+    // Function to delete a node from BST.
+    public static Node deleteNode(Node root, int k) {
+        if(root==null) return null;
+        if(root.data==k) return delete(root);
+        Node curr=root;
+        while(curr!=null){
+            if(k<curr.data){
+                if(curr.left!=null && curr.left.data==k){
+                    curr.left=delete(curr.left);
+                    break;
+                }else{
+                    curr=curr.left;
+                }
+            }else{
+                if(curr.right!=null && curr.right.data==k){
+                    curr.right=delete(curr.right);
+                    break;
+                }else{
+                    curr=curr.right;
+                }
+            }
+        }
+        return root;
+    }
+    
+    public static Node delete(Node node){
+        if(node.left==null){
+            return node.right;
+        }else if(node.right==null){
+            return node.left;
+        }else{
+            Node rc=rightmost(node.left);
+            rc.right=node.right;
+            return node.left;
+        }
+    }
+    
+    public static Node rightmost(Node node){
+        if(node==null) return null;
+        while(node.right!=null){
+            node=node.right;
+        }
+        return node;
+    }
+    
+    
+}
